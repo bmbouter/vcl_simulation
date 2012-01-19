151c99f22a9ea0c20bac5794dfb7d701aaa35577
bmbouter <bmbouter@octave.octave.ncsu.edu>
2012-01-18 20:19:56 -0500

    I think data_file_policy.py is working, but it needs more testing

--- scaler/data_file_policy.py
+++ scaler/data_file_policy.py
@@ -1,4 +1,8 @@
+import csv
+
 from scaler import Scale
+
+DEPROV_CHECK_RATE = 5
 
 class GenericDataFileScaler(Scale):
 
@@ -10,45 +14,63 @@ class GenericDataFileScaler(Scale):
 
     """
 
-    def __init__(self, sim, scale_rate, startup_delay,
-            shutdown_delay, data_path, period_length):
+    def __init__(self, sim, startup_delay,
+            shutdown_delay, data_path):
         """Initializes a GenericDataFileScaler object
 
         parameters:
         sim -- The Simulation containing a cluster cluster object this scale
             function is managing
-        scale_rate -- The interarrival time between scale events in seconds
         startup_delay -- the time a server spends in the booting state
         shutdown_delay -- the time a server spends in the shutting_down state
         data_path -- the full path to the data file
-        period_length -- the period length of the data file
 
         """
 
         self.data_path = data_path
-        self.period_length = period_length
+        self.to_deprovision = 0
+        self.prov_events = self.parse_provisioning_events()
         Scale.__init__(self, sim=sim,
-                             scale_rate=scale_rate,
+                             scale_rate=0,
                              startup_delay=startup_delay,
                              shutdown_delay=shutdown_delay)
+
+    def parse_provisioning_events(self):
+	"""Parses the data file for provisioning and deprovisioning events"""
+        events = []
+        dataReader = csv.reader(open(self.data_path, 'rb'), delimiter=',')
+        for row in dataReader:
+            events.append((float(row[0]), 'P', ))
+            events.append((float(row[1]), 'D', ))
+        return sorted(events)
 
     def scaling_complete(self, stopped_count):
         """Records the number of stopped servers
 
         """
-        self.unprovisioned = self.servers_to_stop - stopped_count
+        self.to_deprovision = self.servers_to_stop - stopped_count
+        if self.to_deprovision > 0:
+            self.prov_events.append(('C', self.sim.now() + DEPROV_CHECK_RATE, ))
+            sorted(self.prov_events)
+
+    def sleep(self):
+        """Overrides the sleep function to follow the data files sleep length"""
+        return self.prov_events[0][0] - self.sim.now()
 
     def scaler_logic(self):
         """Implements the scaler logic specific to this scaler
 
         """
-        dataReader = csv.reader(open(self.data_path, 'rb'), delimiter=',')
-        for row in dataReader:
-            interarrival_time = float(row[0])
-            service_time = float(row[1])
-            yield hold,self,interarrival_time
-            L = User("User %s" % user_num, sim=self.sim)
-            self.sim.activate(L, L.execute(service_time, self.sim.cluster), delay=0)
-            user_num = user_num + 1
+        servers_to_stop = 0
+        servers_to_start = 0
+        while (self.sim.now() == self.prov_events[0][0]):
+            event = self.prov_events.pop(0)
+            event_label = event[1]
+            if event_label == 'P':
+                servers_to_start = servers_to_start + 1
+            elif event_label == 'D':
+                servers_to_stop = servers_to_stop + 1
+            if self.to_deprovision > 0:
+                servers_to_stop = servers_to_stop  + self.to_deprovision
         self.servers_to_stop = servers_to_stop
         return servers_to_start, servers_to_stop
